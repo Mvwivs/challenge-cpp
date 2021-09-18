e22f89deabf327346d039924396b18b497a33476
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2021-09-18 21:03:28 +0300

    add task example test, fix karma, add comments

--- karma.hpp
+++ karma.hpp
@@ -17,17 +17,20 @@ int maxKarma(int k, std::vector<std::pair<int, bool>> contests) {
     std::size_t important_count = std::count_if(contests.begin(), contests.end(), 
         [] (const auto& v) { return v.second; });
 
-    if (k == 0) {
+    if (k == 0) { // only not important events can be lost
         return std::accumulate(contests.begin(), contests.end(), 0, 
-            [] (const auto& a, const auto& v) { return v.second ? a : a + v.first ; });
+            [] (const auto& a, const auto& v) { return v.second ? (a - v.first) : (a + v.first); });
     }
-    else if (important_count <= k) {
+    else if (important_count <= k) { // all events can be lost
 	    return std::accumulate(contests.begin(), contests.end(), 0, 
             [] (const auto& a, const auto& v) { return a + v.first; });
     }
-    else {
+    else { // some events can be lost
+        // find kth largest event among important ones
         std::nth_element(contests.begin(), contests.begin() + k - 1, contests.end(),
             [](const auto& a, const auto& b) {
+                // next three conditions are for not important events,
+                // they are always less than imported ones
                 if (!a.second && !b.second) {
                     return a.first > b.first;
                 }
@@ -37,22 +40,38 @@ int maxKarma(int k, std::vector<std::pair<int, bool>> contests) {
                 if (!b.second) {
                     return true;
                 }
+                // compare important events
                 return a.first > b.first;
         });
-        int kth = contests[k].first;
+        int kth = contests[k - 1].first;
 
+        // additionally there can be several elements equal to k-th,
+        // we need to find how many of them can be lost or won
         std::size_t greater_kth_count = 0;
+        std::size_t less_kth_count = 0;
         int total_karma = 0;
         for (const auto& [karma, important] : contests) {
-            if (important && (karma > kth)) {
-                total_karma += karma;
-                ++greater_kth_count;
+            if (important) {
+                if (karma > kth) { // lost important event
+                    total_karma += karma;
+                    ++greater_kth_count;
+                }
+                else if (karma < kth) { //  won important event
+                    ++less_kth_count;
+                    total_karma -= karma;
+                }
             }
-            else if (!important) {
+            else if (!important) { // sum all not important events
                 total_karma += karma;
             }
         }
-        total_karma += (k - greater_kth_count) * kth;
+        std::size_t kth_count = important_count - less_kth_count - greater_kth_count;
+        std::size_t lost_kth = k - greater_kth_count;
+        std::size_t won_kth = kth_count - lost_kth;
+        // take into account events with karma equal to k-th event
+        total_karma += (lost_kth - won_kth) * kth;
         return total_karma;
     }
+    // Total complexity O(N):
+    // count important events O(N), find k-th element O(N) on average, sum karma O(N),
 }
--- main.cpp
+++ main.cpp
@@ -33,8 +33,8 @@ void test_all_important() {
         {5, true},
         {6, true}
     };
-    assert(maxKarma(0, contests) == 0);
-    assert(maxKarma(1, contests) == 10);
+    assert(maxKarma(0, contests) == -24);
+    assert(maxKarma(1, contests) == -4);
     assert(maxKarma(5, contests) == 24);
 }
 
@@ -51,7 +51,7 @@ void test_one_important() {
     std::vector<std::pair<int, bool>> contests = {
         {2, true},
     };
-    assert(maxKarma(0, contests) == 0);
+    assert(maxKarma(0, contests) == -2);
     assert(maxKarma(1, contests) == 2);
 }
 
@@ -60,7 +60,7 @@ void test_two_different() {
         {2, true},
         {3, false},
     };
-    assert(maxKarma(0, contests) == 3);
+    assert(maxKarma(0, contests) == 1);
     assert(maxKarma(1, contests) == 5);
     assert(maxKarma(2, contests) == 5);
 }
@@ -78,7 +78,18 @@ void test_small_important_cnt() {
     assert(maxKarma(5, contests) == expected);
 }
 
+void test_task_example() {
+    std::vector<std::pair<int, bool>> contests = {
+        {4, true},
+        {2, true},
+        {6, false},
+    };
+    assert(maxKarma(2, contests) == 12);
+    assert(maxKarma(1, contests) == 8);
+}
+
 int main() {
+	test_task_example();
 	test_empty();
 	test_all_not_important();
 	test_all_important();
